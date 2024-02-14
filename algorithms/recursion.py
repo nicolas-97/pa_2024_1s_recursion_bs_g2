8769b630b445b3079bdf5c3a6c0273037f3b07d4
camilo <juan.pedraza2805@gmail.com>
2024-02-14 18:54:52 -0500

    First commit

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -1,29 +1,38 @@
-'''
-    Implementa una función recursiva para calcular el factorial de un número entero positivo
-'''
+# Función recursiva para calcular el factorial de un número entero positivo
 def factorial(n):
-    return n
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-'''
-    Implementa una función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci.
-'''
+# Función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci
 def fibonacci(n):
-    return n
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
-'''
-    Implementa una función recursiva para calcular la suma de los primeros n números enteros.
-'''
+# Función recursiva para calcular la suma de los primeros n números enteros
 def sum_of_numbers(n):
-    return n
+    if n == 0:
+        return 0
+    else:
+        return n + sum_of_numbers(n-1)
 
-'''
-    Implementa una función recursiva para calcular a^n (a elevado a la potencia n).
-'''
+# Función recursiva para calcular a^n (a elevado a la potencia n)
 def power(a, n):
-    return a + n
+    if n == 0:
+        return 1
+    else:
+        return a * power(a, n-1)
 
-'''
-    Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
-'''
+# Función recursiva para encontrar el máximo elemento en una lista de enteros
 def max_in_list(lst):
-    return lst
+    if len(lst) == 1:
+        return lst[0]
+    else:
+        return max(lst[0], max_in_list(lst[1:]))
