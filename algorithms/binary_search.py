47e0a6a3907b4974568bf6cb392fd49464b92f07
Brayan Bellaizan <debyj.leonr@utadeo.edu.co>
2024-02-14 19:15:12 -0500

    Prueba1

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -3,17 +3,52 @@ from models.tree_node import TreeNode
 '''
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
-def binary_search(arr: list, target: int):
-    return 1
+def binary_search(arr: list, target: int) -> int:
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
 
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
-def binary_search_matrix(matrix: list[list[int]], target: int):
-    return False
+
+def binary_search(arr: list, target: int) -> int:
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
-def binary_search_tree(root: TreeNode, target: int):
+def binary_search_tree(root: TreeNode, target: int) -> bool:
+    if not root:
+        return False
+    
+    current = root
+    while current:
+        if current.val == target:
+            return True
+        elif current.val < target:
+            current = current.right
+        else:
+            current = current.left
+    
     return False
