08d43f3e97fbbf0a804fb975afb8fea45d265049
camilo <juan.pedraza2805@gmail.com>
2024-02-14 19:44:37 -0500

    finished function binary_search_tree

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -52,4 +52,11 @@ def binary_search_matrix(matrix: list[list[int]], target: int):
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
-  return False
+    if root==None:
+        return False
+    elif target==root.val:
+        return True
+    elif root.val<target:
+        return binary_search_tree(root.right, target)
+    elif root.val>target:
+        return binary_search_tree(root.left, target)
