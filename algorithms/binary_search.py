020088999170634cfd12954216eb11a4a201fd07
unknown <usuario1@M2-305-17-11376.utadeo.loc>
2024-02-14 19:57:59 -0500

    Cambios taller

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -4,16 +4,48 @@ from models.tree_node import TreeNode
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
 def binary_search(arr: list, target: int):
-    return 1
+    left= 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] ==  target:
+            return mid
+        elif arr[mid] < target:
+            left=mid+1
+        else:
+            right = mid -1    
+    return -1
 
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_matrix(matrix: list[list[int]], target: int):
+    rows, cols = len(matrix), len(matrix)
+    low, high=0, rows * cols -1
+
+    while low <= high:
+        mid=(low+high)//2
+        row, col = divmod(mid,cols)
+        mid_val = matrix[row][col]
+        if mid_val == target:
+            return True
+        elif mid_val < target:
+            low = mid +1
+        else:
+            high = mid - 1
     return False
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
+    if root is None:
+        return False
+    
+    if root.val==target:
+        return True
+    elif root.val<target:
+        return binary_search_tree(root.right,target)
+    else:
+        return binary_search_tree(root.left,target)
     return False
--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -2,28 +2,53 @@
     Implementa una función recursiva para calcular el factorial de un número entero positivo
 '''
 def factorial(n):
-    return n
+     if n==0 or n==1:
+        return 1
+     elif n>1:
+      return n* factorial(n-1)
 
 '''
     Implementa una función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci.
 '''
 def fibonacci(n):
-    return n
+    if n==0:
+       return 0
+    elif n==1:
+       return 1
+    else:
+     return fibonacci(n-1)+fibonacci(n-2)
 
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
 '''
 def sum_of_numbers(n):
-    return n
+    if n <= 0:
+        return 0
+    else: 
+     return n+ sum_of_numbers(n-1)
 
 '''
     Implementa una función recursiva para calcular a^n (a elevado a la potencia n).
 '''
 def power(a, n):
-    return a + n
+      if n == 0:
+        return 1
+      else:
+       return a * power(a, n-1)
 
 '''
     Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
 '''
 def max_in_list(lst):
-    return lst
+    if not lst:
+        return None
+    elif len(lst) == 1:
+        return lst[0]
+    else:
+        head = lst[0]
+        tail = lst[1:]
+        max_tail = max_in_list(tail)
+        if head > max_tail:
+            return head
+        else:
+         return lst
