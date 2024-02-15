b70cabefcd96262071eab165ffe83e3bb5d40804
Brayan Bellaizan <debyj.leonr@utadeo.edu.co>
2024-02-14 20:15:41 -0500

    Prueba2

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -13,25 +13,40 @@ def factorial(n):
 '''
 def fibonacci(n):
     if n==0:
+        return 0
+    elif n==1:
         return 1
-    elif n != 0:
-        return n + (n - 1)
-    return n
+    else:
+        return fibonacci(n-1) + fibonacci (n-2)
 
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
 '''
 def sum_of_numbers(n):
-    return n
+    if n == 0:
+        return 0
+    else:
+        return n + sum_of_numbers(n - 1)
 
 '''
     Implementa una función recursiva para calcular a^n (a elevado a la potencia n).
 '''
 def power(a, n):
-    return a + n
+    if n == 0:
+        return 1
+    else:
+        return a * power(a, n - 1)
 
 '''
     Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
 '''
 def max_in_list(lst):
-    return lst
+    if not lst:
+        return None
+    elif len(lst) == 1:
+        return lst[0]
+    else:
+        mid = len(lst) // 2
+        left_max = max_in_list(lst[:mid])
+        right_max = max_in_list(lst[mid:])
+        return max(left_max, right_max)
