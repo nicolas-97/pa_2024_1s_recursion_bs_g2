3a7ff2451e04d39dbeb5992e1535a73073591846
camilo <juan.pedraza2805@gmail.com>
2024-02-14 19:35:30 -0500

    finished fuction binary_search_matrix

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -18,11 +18,38 @@ def binary_search(arr: list, target: int) -> int:
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
+
 def binary_search_matrix(matrix: list[list[int]], target: int):
-    return False
+    rows=len(matrix)
+    if rows==0: 
+        return False
+
+    mid_rows= rows//2
+    if rows==1: 
+        if len(matrix[0])==0: 
+            return False
+        else: 
+            if binary_search(matrix[mid_rows], target)==-1: 
+                return False
+            else:
+                return True
+    else:
+        print(mid_rows)
+        columns = len(matrix[mid_rows])-1
+        if matrix[mid_rows][0]>target: 
+            return binary_search_matrix(matrix[:mid_rows], target) 
+
+        elif matrix[mid_rows][columns]<target: 
+            return binary_search_matrix(matrix[mid_rows:], target)
+        else: 
+            if binary_search(matrix[mid_rows], target)==-1: 
+                return False
+            else:
+                return True
+
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
-    return False
+  return False
