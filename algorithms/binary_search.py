df61a7f1958ac09ee9f9473179492509b1e11b26
Brayan Camilo Romero Nova <brayan2402romero@gmail.com>
2024-02-18 01:03:24 -0500

    :(

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -6,18 +6,18 @@ from models.tree_node import TreeNode
 def binary_search(arr: list, target: int,left = 0 ,rigth = 0):
 
     if left+rigth == 0:
-        rigth = len(arr)
-
-    if rigth<=left:
+        rigth = len(arr)-1
+        
+    if len(arr) == 0:
+        return -1
+    elif rigth>=left:
         middle = (left + rigth)//2
         if arr[middle]==target:
             return middle
         elif arr[middle] > target:
             return binary_search(arr,target, left, middle-1)
-        elif arr[middle] < target:
-            return binary_search(arr,target, middle+1, rigth)
         else:
-            return -1
+            return binary_search(arr,target, middle+1, rigth)
     else:
         return -1
     
@@ -25,14 +25,35 @@ def binary_search(arr: list, target: int,left = 0 ,rigth = 0):
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_matrix(matrix: list[list[int]], target: int):
-    for i in range(len(matrix)):
-        if binary_search(matrix[i],target) == -1:
+    rows=len(matrix)
+    if rows==0: 
+        return False
+
+    mid_rows= rows//2
+    if rows==1: 
+        if len(matrix[0])==0: 
             return False
-        else:
-            return True        
+        else: 
+            if binary_search(matrix[mid_rows], target)==-1: 
+                return False
+            else:
+                return True
+    else:
+        print(mid_rows)
+        columns = len(matrix[mid_rows])-1
+        if matrix[mid_rows][0]>target: 
+            return binary_search_matrix(matrix[:mid_rows], target) 
+
+        elif matrix[mid_rows][columns]<target: 
+            return binary_search_matrix(matrix[mid_rows:], target)
+        else: 
+            if binary_search(matrix[mid_rows], target)==-1: 
+                return False
+            else:
+                return True
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
-    return False
+        return False
