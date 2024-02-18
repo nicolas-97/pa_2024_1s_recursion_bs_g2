b18f86d35dbe6e1f321b88408ee275135d8f7941
JairoHRM <jairo.rodriguez@utadeo.edu.co>
2024-02-17 23:17:22 -0500

    prueba binary

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -3,21 +3,29 @@ from models.tree_node import TreeNode
 '''
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
-def binary_search(arr, menor, mayor, buscado):
-     
-    if mayor >= menor:
+def binary_search(arr, numero):
+    menor = 0
+    alto = len(arr) - 1
+    med = 0
  
-        mid = (mayor + menor) // 2
+    while menor <= alto:
  
-        if arr[mid] == buscado:
-            return mid
-        elif arr[mid] > buscado:
-            return binary_search(arr, menor, mid - 1, buscado)
+        med = (alto + menor) // 2
+ 
+        # If numero is greater, ignore left half
+        if arr[med] < numero:
+            menor = med + 1
+ 
+        # If numero is smaller, ignore right half
+        elif arr[med] > numero:
+            alto = med - 1
+ 
+        # means numero is present at med
         else:
-            return binary_search(arr, mid + 1, mayor, buscado)
+            return med
  
-    else:
-        return -1
+    # If we reach here, then the element was not present
+    return -1
 
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
