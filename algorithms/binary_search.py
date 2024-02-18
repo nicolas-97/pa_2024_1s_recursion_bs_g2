fd2eb07022432e348239dc66bb0aa5e17ff5d768
Brayan Camilo Romero Nova <brayan2402romero@gmail.com>
2024-02-18 00:24:53 -0500

    Aclaración

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -4,21 +4,29 @@ from models.tree_node import TreeNode
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
 def binary_search(arr: list, target: int,left = 0 ,rigth = 0):
-  
+
     if left+rigth == 0:
         rigth = len(arr)
 
-    if rigth >= left:  
+    middle = (left + rigth)//2
 
-        middle = (left + rigth)//2
-
+    if len(arr) == 1:
         if arr[middle]==target:
             return middle
-        elif arr[middle] > target:
-            return binary_search(arr,target, left, middle-1)
         else:
-            return binary_search(arr,target, middle+1, rigth)
-    else:
+            return -1
+    elif len(arr)>1:
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            rigth = middle-1
+            return binary_search(arr,target, left, rigth)
+        elif arr[middle] < target:
+            left = middle+1
+            return binary_search(arr,target, left, rigth)
+        else:
+            return -1
+    elif len(arr) == 0:
         return -1
     
 '''
