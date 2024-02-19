86c570f1517353a8b95b0caa14c5961eda4f6197
nicolasmahecha1125 <nicolasmahecha1125@gmail.com>
2024-02-18 21:44:21 -0500

    1

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -4,57 +4,21 @@ from models.tree_node import TreeNode
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
 def binary_search(arr: list, target: int):
-    def bynary_search_recursive(arr, target, low, high):
-        if low <= high:
-            mid= (low + high)//2
-            if arr[mid] == target:
-                return mid
-            elif arr[mid] < target:
-                return bynary_search_recursive(arr , target, mid + 1, high)
-            else:
-                return bynary_search_recursive(arr, target, low, mid - 1)
-        else:
-            return -1
-    return bynary_search_recursive(arr, target, 0, len(arr)-1)
+
+    return -1
+    
 
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_matrix(matrix: list[list[int]], target: int):
-    filas= len(matrix)
-    columnas = len(matrix[0])
-
-    inicio = 0
-    fin = filas * columnas - 1
-    while inicio <= fin:
-        medio = (inicio + fin) // 2
-        fila = medio // columnas
-        columna = medio % columnas
-        
-        if matrix[fila][columna] == target:
-            return True
-        elif matrix[fila][columna] < target:
-            inicio = medio + 1
-        else:
-            fin = medio - 1
-
+    
     return False
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-def binary_search_tree(root: TreeNode, target: int) ->bool:
-    if not root:
-        return False
-    if root.val == target:
-        return True
-    elif root.val < target:
-        return binary_search_tree(root.right, target)
-    else:
-        return binary_search_tree(root.left, target)
+def binary_search_tree(root: TreeNode, target: int):
+    
+    return False
     
--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -2,52 +2,33 @@
     Implementa una función recursiva para calcular el factorial de un número entero positivo
 '''
 def factorial(n):
-    fact = 1
-    for i in range(2,n+1):
-        fact *= i
     return n
 
 '''
     Implementa una función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci.
 '''
 def fibonacci(n):
-    if n <=0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+
+    return n
 
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
 '''
 def sum_of_numbers(n):
-    if n ==0:
-        return 0
-    else:
-        return n + sum_of_numbers(n-1)
+    
+    return n 
 
 
 '''
     Implementa una función recursiva para calcular a^n (a elevado a la potencia n).
 '''
 def power(a, n):
-    if n == 0:
-        return 1
-    elif n ==1:
-        return a
-    else:
-        return a * power(a,n-1)
+    
+    return a + n
 
 '''
     Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
 '''
 def max_in_list(lst):
-    if len(list) == 1:
-        return list[0]
-    else:
-        maximo = max_in_list(list[1:])
-        if list[0] > maximo:
-            return list[0]
-        else:
-            return maximo
+    
+    return lst
