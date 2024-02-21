1e40a1ee06afb406f2011a8c07fd693f0af17c7b
federios26 <158357674+federios26@users.noreply.github.com>
2024-02-21 18:56:29 -0500

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -9,7 +9,8 @@ def fibonacci(n):
     if n <= 1:
         return n
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2))
+        return (fibonacci(n - 1) + fibonacci(n - 2))
+
 
 def max_in_list(lst):
     if not lst:
