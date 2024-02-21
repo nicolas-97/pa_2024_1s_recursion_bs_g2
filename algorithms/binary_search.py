ae092f8375d3f91a8f8652f9217a1c5882d30cc0
Brayan Camilo Romero Nova <brayan2402romero@gmail.com>
2024-02-20 22:05:05 -0500

    Ejercicios

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -3,57 +3,51 @@ from models.tree_node import TreeNode
 '''
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
-def binary_search(arr: list, target: int,left = 0 ,rigth = 0):
+def binary_search(arr: list, target: int):
 
-    if left+rigth == 0:
-        rigth = len(arr)-1
-        
-    if len(arr) == 0:
-        return -1
-    elif rigth>=left:
-        middle = (left + rigth)//2
-        if arr[middle]==target:
-            return middle
-        elif arr[middle] > target:
-            return binary_search(arr,target, left, middle-1)
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
         else:
-            return binary_search(arr,target, middle+1, rigth)
-    else:
-        return -1
+            right = mid - 1
+    return -1
     
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_matrix(matrix: list[list[int]], target: int):
-    rows=len(matrix)
-    if rows==0: 
-        return False
+    row = 0
 
-    mid_rows= rows//2
-    if rows==1: 
-        if len(matrix[0])==0: 
+    if matrix == []: return False
+
+    while binary_search(matrix[row], target) == -1:
+        row += 1
+        if row == len(matrix):
             return False
-        else: 
-            if binary_search(matrix[mid_rows], target)==-1: 
-                return False
-            else:
-                return True
-    else:
-        print(mid_rows)
-        columns = len(matrix[mid_rows])-1
-        if matrix[mid_rows][0]>target: 
-            return binary_search_matrix(matrix[:mid_rows], target) 
-
-        elif matrix[mid_rows][columns]<target: 
-            return binary_search_matrix(matrix[mid_rows:], target)
-        else: 
-            if binary_search(matrix[mid_rows], target)==-1: 
-                return False
-            else:
-                return True
-
+    return True
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
+    if root == None: 
+        return False
+    
+    actual_node = root.val
+
+    if actual_node == target:
+        return True
+
+    while actual_node != target:
+
+        if actual_node > target:
+           return binary_search_tree(root.left, target)
+
+        if actual_node < target :
+            return binary_search_tree(root.right, target)
+
         return False
--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -12,7 +12,10 @@ def factorial(n):
     Implementa una función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci.
 '''
 def fibonacci(n):
-    return n
+    if(n<=1):
+        return n
+    else:
+        fibonacci(n-1) + fibonacci(n-2)
 
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
@@ -38,5 +41,9 @@ def power(a, n):
     Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
 '''
 def max_in_list(lst):
+    if len(lst)==1:
+        return lst[0]
     
-    return lst
+    else:
+        max_number = max_in_list(lst[1:])
+        return max_number if max_number > lst[0] else lst[0]
