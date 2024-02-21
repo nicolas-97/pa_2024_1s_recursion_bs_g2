a359c75b3b2c4487e3cf4819bc37d44632f9fd4e
Andres-Gz <afgm.913@hotmail.com>
2024-02-21 14:24:56 -0500

    | TASK-2 | 1.0.0 | - Se agregan las funciones deseadas para implementar recursividad y busqueda binaria

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,19 +1,63 @@
-from models.tree_node import TreeNode
+from ..models.tree_node import TreeNode
 
 '''
     Implementa una función de búsqueda binaria que encuentre un elemento en una lista ordenada de enteros y retorne su posicion si no lo encuntra que retorne -1
 '''
+
+# arr = [1,2,3]
+# target = 0
 def binary_search(arr: list, target: int):
-    return 1
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid  # Elemento encontrado, retornamos su posición
+        elif arr[mid] < target:
+            left = mid + 1  # Búsqueda en la mitad derecha
+        else:
+            right = mid - 1  # Búsqueda en la mitad izquierda
+
+    return -1  # Elemento no se encuentra en la lista
 
 '''
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_matrix(matrix: list[list[int]], target: int):
+    if not matrix:
+        return False
+
+    nrows = len(matrix)
+    ncols = len(matrix[0])
+
+    left = 0
+    right = nrows * ncols - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        mid_value = matrix[mid // ncols][mid % ncols]
+
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
     return False
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
-    return False
+    if root is None or root.val == target:
+        return root is not None
+
+    # El valor del nodo actual es mayor entonces ir al subárbol izquierdo
+    if root.val > target:
+        return binary_search_tree(root.left, target)
+
+    # O ir al subárbol derecho
+    return binary_search_tree(root.right, target)
