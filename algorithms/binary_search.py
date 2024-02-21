16e82e08a35236a5c8ab188f9ddeced7bf628a4e
JairoHRM <jairo.rodriguez@utadeo.edu.co>
2024-02-20 22:54:15 -0500

    pruebas

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -31,10 +31,32 @@ def binary_search(arr, numero):
     Implementa una función de búsqueda binaria para encontrar un elemento en una matriz ordenada (fila y columna) de enteros y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_matrix(matrix: list[list[int]], target: int):
+    for fila_index, fila in enumerate(matrix):
+        for col_index, valor in enumerate(fila):
+            if valor == target:
+                return True
     return False
+
 
 '''
     Implementa una función de búsqueda binaria en un árbol binario de búsqueda  y retorne verdadero si la encuntra el elmento o falso si no lo encuentra
 '''
 def binary_search_tree(root: TreeNode, target: int):
-    return False
+    if root == None: 
+        return False
+    
+    actual_node = root.val
+
+    if actual_node == target:
+        return True
+
+    while actual_node != target:
+
+        if actual_node > target:
+           return binary_search_tree(root.left, target)
+
+        if actual_node < target :
+            return binary_search_tree(root.right, target)
+
+        return False
+
