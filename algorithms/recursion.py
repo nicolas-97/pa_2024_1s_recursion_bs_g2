a6d8aed63f21b051bffd8fe3de7ecdf93f500a22
federios26 <158357674+federios26@users.noreply.github.com>
2024-02-21 18:54:29 -0500

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -4,33 +4,24 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-def fibonacci(n):
 
+def fibonacci(n):
     if n <= 1:
         return n
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci(n - 1) + fibonacci(n - 2))
 
 def max_in_list(lst):
-    if len(lst) == 1:
+    if not lst:
+        return None 
+    elif len(lst) == 1:
         return lst[0]
     else:
-        return max(lst[0], max_in_list(lst[1:]))
-
-
-    return n
-
-'''
-    Implementa una función recursiva para calcular la suma de los primeros n números enteros.
-'''
-def sum_of_numbers(n):
-    
-    return n 
-
+        max_rest = max_in_list(lst[1:])
+        return lst[0] if lst[0] > max_rest else max_rest
 
 
 def power(a, n):
-
     if n == 0:
         return 1
     else:
@@ -40,19 +31,12 @@ def sum_of_numbers(n):
     if n == 0:
         return 0
     else:
-        return n + sum_of_numbers(n - 1)
+        return n + sum_of_numbers(n - 1))
 
 
 
         
 
     
-    return a + n
 
-'''
-    Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
-'''
-def max_in_list(lst):
-    
-    return lst
 
