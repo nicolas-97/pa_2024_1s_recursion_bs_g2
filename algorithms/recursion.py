7196599768bf1354cdac65ddc6398bf14df4dc40
JairoHRM <jairo.rodriguez@utadeo.edu.co>
2024-02-20 23:14:57 -0500

    definitivo

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -3,6 +3,11 @@
 '''
 def factorial(n):
     return n
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 '''
     Implementa una función recursiva para calcular el término n-ésimo de la secuencia de Fibonacci.
@@ -10,20 +15,52 @@ def factorial(n):
 def fibonacci(n):
     return n
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
 '''
     Implementa una función recursiva para calcular la suma de los primeros n números enteros.
 '''
 def sum_of_numbers(n):
-    return n
+    if n <= 0:
+        return 0
+    else:
+        return n + sum_of_numbers(n - 1)
 
 '''
-    Implementa una función recursiva para calcular a^n (a elevado a la potencia n).
+    Implementa una función recursiva para calcular a^n (a elevado a la power n).
 '''
+
 def power(a, n):
-    return a + n
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 1 / power(a, -n)
+    elif n % 2 == 0:
+        return power(a * a, n // 2)
+    else:
+        return a * power(a, n - 1)
 
 '''
-    Implementa una función recursiva para encontrar el máximo elemento en una lista de enteros.
+    Implementa una función recursiva para encontrar el máximo elemento en una lst de enteros.
 '''
 def max_in_list(lst):
-    return lst
+    if not lst:
+        return None
+    
+    if len(lst) == 1:
+        return lst[0]
+    
+    mitad = len(lst) // 2
+    izquierda = max_in_list(lst[:mitad])
+    derecha = max_in_list(lst[mitad:])
+    
+    if izquierda is None:
+        return derecha
+    elif derecha is None:
+        return izquierda
+    else:
+        return max(izquierda, derecha)
